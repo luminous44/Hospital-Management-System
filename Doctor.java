98fc6fa9e3fba1a8bc97b1bbcb6488c4089b4a3c
luminous44 <aklemasultanakhi@gmail.com>
2025-04-27 14:31:37 +0600

    added doctor details

---
+++ Doctor.java
@@ -0,0 +1,64 @@
+package hospitalmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Doctor {
+
+    private Connection connection;
+    private Scanner scanner;
+
+    Doctor(Connection con, Scanner sc) {
+        this.connection = con;
+        this.scanner = sc;
+    }
+
+    // view all doctors details
+    public void viewDoctors() {
+        try {
+            String query = "select * from doctors";
+            PreparedStatement pre = connection.prepareStatement(query);
+            ResultSet resultSet = pre.executeQuery();
+            System.out.println("+---------------+-------------------------+--------------------+");
+            System.out.println("| Doctors Id    | Name                    | Specialization     |");
+            System.out.println("+---------------+-------------------------+--------------------+");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialization = resultSet.getString("specialization");
+                System.out.printf("|%-15s|%-25s|%-20s|\n", id, name, specialization);
+            }
+            System.out.println("+---------------+-------------------------+--------------------+");
+        } catch (SQLException e) {
+            System.out.println("Failed to load doctors details" + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    // check doctor
+    public boolean getDoctorById(int dId) {
+
+        String Query = "SELECT * FROM doctors WHERE id =?";
+        try {
+            PreparedStatement pre = connection.prepareStatement(Query);
+            pre.setInt(1, dId);
+            ResultSet resultSet = pre.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to load doctor information " + e.getMessage());
+            e.printStackTrace();
+
+        }
+        return false;
+
+    }
+
+}
